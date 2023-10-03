75a3c38af9f7c63bdb6a58b4290b1fb2e18a8e60
leentaylor <leentaylor@gmail.com>
2023-10-04 10:49:07 +1100

    configure extra parameters and parameter sanitization from devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+protected
+def configure_permitted_parameters
+  # will this work? this is what rubocop wants instead of keys: [:username :phone_number :full_name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username phone_number full_name])
+  end
 end
