a33d0b9beb07135223e1e6fcdb771c6688ca91f0
leentaylor <leentaylor@gmail.com>
2023-10-08 11:30:48 +1100

    add authenticate user as before action in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 protected
 def configure_permitted_parameters
   # will this work? this is what rubocop wants instead of keys: [:username :phone_number :full_name]
