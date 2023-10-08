b482dc31345c767385dddbab372fd548ca5e8024
leentaylor <leentaylor@gmail.com>
2023-10-09 10:47:19 +1100

    configure filepond to upload images from a specific selector

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -1,1 +1,11 @@
 import * as FilePond from "filepond";
+// Get a reference to the file input element
+const inputElement = document.querySelector("#post-images");
+
+// Create a FilePond instance
+const pond = FilePond.create(inputElement, {
+  storeAsFile: true,
+  credits: {},
+  allowMultiple: true,
+  allowReorder: true,
+});
