0ee1897452204796414d78ff0b24462fb955bc23
leentaylor <leentaylor@gmail.com>
2023-10-09 13:33:00 +1100

    use turbo:load event to run filepond on each page load (eg with client side routing)

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -2,19 +2,23 @@ import * as FilePond from "filepond";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 
-// Register the plugin
-FilePond.registerPlugin(FilePondPluginImagePreview);
-FilePond.registerPlugin(FilePondPluginFileValidateType);
+document.addEventListener("turbo:load", loadFilePond);
 
-// Get a reference to the file input element
-const inputElement = document.querySelector("#post-images");
+function loadFilePond() {
+  // Register the plugin
+  FilePond.registerPlugin(FilePondPluginImagePreview);
+  FilePond.registerPlugin(FilePondPluginFileValidateType);
 
-// Create a FilePond instance
-const pond = FilePond.create(inputElement, {
-  storeAsFile: true,
-  credits: {},
-  allowMultiple: true,
-  allowReorder: true,
-  acceptedFileTypes: ["image/*"],
-  required: true,
-});
+  // Get a reference to the file input element
+  const inputElement = document.querySelector("#post-images");
+
+  // Create a FilePond instance
+  const pond = FilePond.create(inputElement, {
+    storeAsFile: true,
+    credits: {},
+    allowMultiple: true,
+    allowReorder: true,
+    acceptedFileTypes: ["image/*"],
+    required: true,
+  });
+}
