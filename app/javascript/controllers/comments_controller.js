7a7d76cbec192943fb422291fc9fde112c57a2f7
leentaylor <leentaylor@gmail.com>
2023-10-11 11:20:01 +1100

    remove debugging code

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -1,16 +1,8 @@
 import { Controller, Application } from "@hotwired/stimulus";
 
-const application = Application.start();
-application.debug = true;
-
 export default class extends Controller {
 
-  connect() {
-    console.log("Hello, Stimulus!", this.element)
-  }
-
   clear() {
-    console.log("clear was called")
     this.element.reset();
   }
 }
